2572cccdd1210d70e34b312f37c64c9018b1aacf
Lior Shitrit <liorsh@tavmedical.com>
2019-09-05 11:50:25 +0300

    added npm audit fix to install dep. batch

--- install.bat
+++ install.bat
@@ -1,3 +1,7 @@
 @ECHO OFF
-ECHO Installing Dependencies
+ECHO Installing Dependencies...
 npm install
+ECHO Dependencies Installed
+ECHO ------------------------------------
+ECHO Running Audit Fix
+npm audit fix
